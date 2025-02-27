44e84ac07dfe65085893163d058aa40917d6e233
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-27 12:25:33 +0300

    Ветка 2.5

--- src/App.vue
+++ src/App.vue
@@ -29,12 +29,12 @@
     <v-app-bar app dark color="primary">
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-sm-and-down">
         <v-toolbar-items class="hidden-sm-and-down">
 
           <v-btn v-for="link in links" :key="link.title"><v-icon start :icon="link.icon"></v-icon>
             {{ link.title }}
           </v-btn>
+        </v-toolbar-items>  
     </v-app-bar>
     <v-main>
     </v-main>
