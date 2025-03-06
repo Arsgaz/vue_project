74d77172dd59204d5c939f2658378fa744f28b76
Kiselyaka <kiselyaka12s@gmail.com>
2025-03-06 12:25:33 +0300

    Пункт 14.2

--- src/views/Auth/LoginView.vue
+++ src/views/Auth/LoginView.vue
@@ -50,16 +50,22 @@ export default {
         }
     },
     methods: {
-	onSubmit(){
-        if (this.$refs.form.validate()){
-            const user = {
-				email: this.email,
-				password: this.password
-			}
-			console.log(user)
+        onSubmit(){
+	if (this.$refs.form.validate()){
+		const user = {
+			email: this.email,
+			password: this.password
+		}
+		this.$store.dispatch('loginUser', user)
+		.then(() => {
+			this.$router.push("/")
+		})
+		.catch((err) => {
+			console.log(err)
+		})
+	}
 }
 
-	}
 }
 
 } 
